282268f15072ceeb0295ec2a582b04344d8fad9b
Eric Babiarz <145168964+ebabiarz@users.noreply.github.com>
2025-06-28 10:12:30 -0500

    Add ComputerPlayer class. Add method to generate secret code. Add method to convert numbers to colors.

---
+++ lib/computer_player.rb
@@ -0,0 +1,26 @@
+class ComputerPlayer
+  COLOR_CODE = {'0': "blue", '1': "red", '2': "yellow", '3': "orange", '4': "green", '5': "purple"}
+
+  attr_accessor :secret_code
+
+  def initialize
+    @secret_code = self.generate_secret_code
+  end
+
+  def generate_secret_code
+    array = Array.new
+    i = 0
+    while i < 4
+      array.push(Random.rand(6))
+      i += 1
+    end
+    array = convert_to_color(array)
+  end
+
+  def convert_to_color(array)
+    array.each_index do |index|
+      array[index] = COLOR_CODE[:"#{array[index]}"]
+    end
+    return array
+  end
+end
