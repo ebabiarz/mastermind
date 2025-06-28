d194ae85dbd13b8619e6476bbcaa9b01e483135e
Eric Babiarz <145168964+ebabiarz@users.noreply.github.com>
2025-06-28 16:35:38 -0500

    Create Game class with turn_number instance variable.

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+class Game
+
+  attr_accessor :turn_number
+  
+  def initialize
+    @turn_number = 1
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/human_player'
 require_relative 'lib/computer_player'
+require_relative 'lib/game'
 
 
 
